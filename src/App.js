a93e49f1de0154928d61a061f4d431e8e8070010
Deepak Chaula <yess_bose@yahoo.com>
2023-04-12 20:33:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,143 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Container from '@mui/material/Container';
+// import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import SearchIcon from '@mui/icons-material/Search';
+import CircularProgress from '@mui/material/CircularProgress';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import PlanetSearch from './components/PlanetSearch';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  const [selectedPlanet, setSelectedPlanet] = useState("");
+  const [planetResidents, setPlanetResidents] = useState([]);
+  const [inputTxt, setInputTxt] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+
+  const fetchPlanets = async () => {
+    try {
+      const response = await axios.get("https://swapi.dev/api/planets/");
+      setPlanets(response.data.results);
+    } catch (error) {
+      console.error("Error fetching planets data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  const searchPlanet = async () => {
+    setError(null);
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://swapi.dev/api/people/?search=${inputTxt}`
+      );
+
+      if (response.data.results.length > 0) {
+        const residents = await Promise.all(
+          response.data.results[0].residents
+            .slice(0, 10 * page)
+            .map((resident) => axios.get(resident))
+        );
+        setPlanetResidents(residents.map((r) => r.data.name));
+      } else {
+        setPlanetResidents([]);
+        setError("No residents found for the selected planet");
+      }
+    } catch (error) {
+      console.error("Error fetching planet data:", error);
+      setError("An error occurred while searching for the planet");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadMore = () => {
+    setPage(page + 1);
+    searchPlanet();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Star Wars Planet Search</h1>
-      </header>
-      <main>
-        <PlanetSearch />
-      </main>
-    </div>
-  );
+    <Container maxWidth="sm">
+      <h1>Star Wars Planet Search</h1>
+      <FormControl fullWidth variant="outlined">
+        <InputLabel htmlFor="planet">Planet Name</InputLabel>
+        <Select
+          labelId="planet"
+          id="planet"
+          value={selectedPlanet}
+          onChange={(e) => setSelectedPlanet(e.target.value)}
+          label="Planet Name"
+        >
+          {planets.map((planet, index) => (
+            <MenuItem key={index} value={planet.name}>
+              {planet.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      
+      <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+        marginTop: '1rem',
+      }}
+    >
+    <div style={{display: "flex", alignItems: "center"}}>
+      <TextField fullWidth label="People" size="small" id="fullWidth" />
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<SearchIcon />}
+        onClick={searchPlanet}
+        style={{ marginLeft: "1rem" }}
+      >
+        Search
+      </Button>
+      </div>
+    </Box>
+     
+      {selectedPlanet && <PlanetSearch selectedPlanet={selectedPlanet} />}
+      {loading ? (
+        <CircularProgress style={{ marginTop: "1rem" }} />
+      ) : (
+        <List>
+          {planetResidents.map((resident, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={resident} />
+            </ListItem>
+          ))}
+          {planetResidents.length > 0 && (
+            <ListItem>
+              <Button variant="contained" color="primary" onClick={loadMore}>
+                Load More
+              </Button>
+            </ListItem>
+          )}
+        </List>
+      )}
+      {error && (
+        <Typography color="error" style={{ marginTop: "1rem" }}>
+          {error}
+    </Typography>
+  )}
+</Container>
+);
 }
 
 export default App;
--- src/components/PlanetSearch.js
+++ src/components/PlanetSearch.js
@@ -1,41 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const PlanetSearch = () => {
-  const [planet, setPlanet] = useState('');
+import CircularProgress from '@mui/material/CircularProgress';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const PlanetSearch = ({selectedPlanet}) => {
+  const [planet, setPlanet] = useState();
   const [people, setPeople] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   // Fetch and set data functions here
   const fetchPeople = async (planet, page) => {
+    setLoading(true);
     try {
       const response = await axios.get(`https://swapi.dev/api/people/?page=${page}`);
       const residents = response.data.results;
       setPeople(residents);
       setTotalPages(Math.ceil(response.data.count / 10));
+      setLoading(false);
     } catch (error) {
       console.error(error);
       alert('An error occurred while fetching data.');
     }
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    setCurrentPage(1);
-    fetchPeople(planet, 1);
-  };
+  // const handleSearch = (e) => {
+  //   e.preventDefault();
+  //   setCurrentPage(1);
+  //   fetchPeople(planet, 1);
+  // };
 
   useEffect(()=>{
+    setPlanet(selectedPlanet);
+    console.log(selectedPlanet);
     fetchPeople(planet, 1);
+    
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  },[selectedPlanet])
 
 
   return (
     <div>
       {/* Search UI and people list rendering here */}
-      <form onSubmit={handleSearch}>
-        <label htmlFor="planet">Planet: </label>
+      {/* <form onSubmit={handleSearch}>
+        <label htmlFor="planet">People: </label>
         <input
           type="text"
           id="planet"
@@ -44,12 +55,22 @@ const PlanetSearch = () => {
           required
         />
         <button type="submit">Search</button>
-      </form>
-      <ul>
+      </form> */}
+    {loading ? (
+        <CircularProgress style={{ marginTop: "1rem" }} />
+      ) : (
+        <div style={{display: "flex", width: "100%", flexWrap: "wrap"}}>
         {people.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <Card key={index} sx={{ minWidth: 250 }} style={{ margin: "5px" }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+              {person.name}
+              </Typography>
+            </CardContent>
+          </Card>
         ))}
-      </ul>
+      </div>
+      )}
       <div>
         {currentPage > 1 && (
           <button onClick={() => {
@@ -67,7 +88,7 @@ const PlanetSearch = () => {
             Next
           </button>
         )}
-      </div>
+     </div>
 
     </div>
   );
